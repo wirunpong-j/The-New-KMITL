5e7eeccf6f078a32a5446a79dc15acfb5345938f
Wirunpong Jaingamlertwong <it58070126@office365.it.kmitl.ac.th>
2017-10-05 23:06:41 +0700

    edit fragment and set fragment

--- TheNewKMITL/app/src/main/java/lab/bellkung/thenewkmitl/HomeActivity.java
+++ TheNewKMITL/app/src/main/java/lab/bellkung/thenewkmitl/HomeActivity.java
@@ -1,7 +1,9 @@
 package lab.bellkung.thenewkmitl;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,34 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+
+    public void btnPressed(View view) {
+
+        FragmentTransaction trans = getFragmentManager().beginTransaction();
+
+        switch (view.getId()) {
+            case R.id.feedBtn:
+                trans.replace(R.id.mainFragment, new FeedFragment());
+                trans.addToBackStack(null);
+                trans.commit();
+                break;
+            case R.id.calendarBtn:
+                trans.replace(R.id.mainFragment, new CalendarFragment());
+                trans.addToBackStack(null);
+                trans.commit();
+                break;
+            case R.id.mapBtn:
+                trans.replace(R.id.mainFragment, new MapFragment());
+                trans.addToBackStack(null);
+                trans.commit();
+                break;
+            case R.id.otherBtn:
+                trans.replace(R.id.mainFragment, new OtherFragment());
+                trans.addToBackStack(null);
+                trans.commit();
+                break;
+        }
     }
 }
